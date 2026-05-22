3af48a5327dc1785d0378baf3cd1f4bace8cfe19
Felipe Sakai Ipolito <felipesakaiipolito@gmail.com>
2026-05-22 15:40:20 -0400

    feat: introduce UniqueEntityId class and refactor entities to use it; add Optional type utility

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,16 +1,25 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface QuestionProps {
-    title: string,
-    slug: Slug,
-    content: string,
-    authorId: string
+    authorId: UniqueEntityId
+    bestAnswerId?: UniqueEntityId
+    title: string
+    content: string
+    slug: Slug
+    createdAt: Date
+    updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
-    get content() {
-        return this.props.content
+    static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityId) {
+        const question = new Question(
+            {
+                ...props,
+                createdAt: new Date(),
+            }, id)
+        return question
     }
-
 }
